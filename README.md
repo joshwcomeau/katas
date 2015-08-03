ade097d3dd71a503a933a49c2b69f1f89b186b60
Joshua Comeau <joshwcomeau@gmail.com>
2015-08-03 07:59:28 -0400

    Added an init function that iterates through requested caching values. Started writing caching function.

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Started as a way to exercise my brain and have fun, I'll be solving a programmin
 --------
 
 ###Problem 1: Largest 5-digit consecutive number in a sequence  
-This straight-forward math/programming challenge from CodeWars asks you to find the largest 5-digit number in a series. For example, the largest 2-digit number in '19285' is 92.  
+This straight-forward math/programming challenge from CodeWars asks you to find the largest 5-digit number in a series. For example, the largest 2-digit number in `19285` is 92.  
 [Problem](http://www.codewars.com/kata/51675d17e0c1bed195000001) | [My Solution](https://github.com/joshwcomeau/katas/tree/master/codewars_1) | [Blog Post](https://medium.com/@joshuawcomeau/weekly-challenge-01-93d562c06d6d)
 
 --------
 
 ###Problem 2: LRU Cache
 Also from CodeWars, this problem asks you to create a Least-Recently-Used cache. It's an incredibly enlightening exercise; `Object.defineProperty` is amazing.  
-[Source](http://www.codewars.com/kata/53b406e67040e51e17000c0a/train/javascript)
+[Problem](http://www.codewars.com/kata/53b406e67040e51e17000c0a/train/javascript)
--- codewars_2/v1.js
+++ codewars_2/v1.js
@@ -1,18 +1,36 @@
 // Implementation of LRU Cache. Version 1
-function LRUCache(capacity, init) {
-  var self = this;
+"use strict";
 
-  this.size = 0;
-  // implement capacity getter/setter
-  this.capacityValue = 3;
+function LRUCache(capacity, initial) {
+  this.initialize(capacity, initial);
+  var self = this
 
   Object.defineProperty(this, 'capacity', {
     get: function() { return self.capacityValue; },
     set: function(newVal) { self.capacityValue = newVal; }
   });
+
+  Object.defineProperty(this, 'size', {
+    get: function() { return self.sizeValue; }
+  });
 }
 
-var store = new LRUCache();
-console.log("initial capacity should be 3; it is:", store.capacity);
+LRUCache.prototype.initialize = function(capacity, initial) {
+  // Set our internally tracked values
+  this.sizeValue = 0;
+  this.capacityValue = capacity;
+
+  // Iterate through our initial values, adding them to the cache.
+  for (var prop in initial) {
+    this.cache(prop, initial[prop]);
+  }
+}
+
+LRUCache.prototype.cache = function(key, val) {
+  console.log("Caching", key, "with val of", val);
+}
+
+var store = new LRUCache(5, {a: 5, b:12});
+console.log("initial capacity should be 5; it is:", store.capacity);
 store.capacity = 10;
 console.log("After setting to 10, it is:", store.capacity);
