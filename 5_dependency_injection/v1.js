72f0835a06c2ae74c9560bba4d83af16d3011f75
Joshua Comeau <joshwcomeau@gmail.com>
2016-01-03 13:30:12 -0500

    Work on problem 5

---
+++ 5_dependency_injection/v1.js
@@ -0,0 +1,62 @@
+/**
+ * Constructor DependencyInjector
+ * @param {Object} - object with dependencies
+ */
+var DI = function (dependencies) {
+  this.dependencies = dependencies;
+};
+
+// Should return new function with resolved dependencies
+DI.prototype.inject = function (func) {
+  // First, we need to find our function's parameters in our dependency object.
+  var param_names = getParams(func);
+  
+  // Let's look up those params in our dependencies obj
+  var params = param_names.map( function(name) {
+    return this.dependencies[name];
+  }.bind(this));
+  
+  // Remove any unresolved dependencies from the params array.
+  var scrubbed_params = params.filter( function(param) {
+    return typeof param !== 'undefined';
+  });
+  
+  return function() {
+    return func.apply(this, scrubbed_params);
+  };
+}
+
+
+function getParams(func) {
+  // We want to define a function that takes a function as an argument, and we
+  // want to return an array of its parameters.
+  //
+  // For example, if we passed in a function like this:
+  //    function(param, other_param) { // function stuff here }
+  //
+  // We'd expect the following return:
+  //   ['param', 'other_param']
+  
+  // While Javascript gives us a convenient way for accessing arguments with
+  // the `arguments` keyword, it doesn't give us anything for params.
+  
+  // For fun, I tried console.log-ing a function. The result is surprising:
+  //   function(param, other_param) { // function stuff here }
+  // It literally just returns the _whole_ function definition as a string.
+  
+  // On the assumption that console.log uses the .toString() method, I tried this:
+  var func_string = func.toString();
+  
+  // Using indexOf(), we can find the place of the first '(' and the first ')'.
+  // Adding 1 to opening because we don't want this character itself.
+  var open_parenthesis  = func_string.indexOf('(') + 1; 
+  var close_parenthesis = func_string.indexOf(')');
+  
+  // We can slice everything between them, for a string:
+  // 'param, other_param'
+  var param_string = func_string.slice( open_parenthesis, close_parenthesis );
+  
+  // Now we just need to wipe out any whitespace, and split by commas!
+  return param_string.replace(/\s/g, '').split(',');
+}
+module.exports = DI;
---
+++ test/5_dependency.spec.js
@@ -0,0 +1,25 @@
+var expect = require('chai').expect;
+
+var DI = require('../5_dependency_injection/v1');
+
+describe('Dependency Injector', function() {
+  it('resolves dependencies', function() {
+    var deps = {
+      'dep1': function () {return 'this is dep1';},
+      'dep2': function () {return 'this is dep2';},
+      'dep3': function () {return 'this is dep3';},
+      'dep4': function () {return 'this is dep4';}
+    };
+      
+    var di = new DI(deps);
+
+    var yes = di.inject( function(dep3, dep1, dep2) {
+      return [dep1(), dep2(), dep3()].join(' -> ');
+    });
+    
+    console.log("YES", yes());
+    
+    expect(yes()).to.equal('this is dep1 -> this is dep2 -> this is dep3');
+
+  });
+});
